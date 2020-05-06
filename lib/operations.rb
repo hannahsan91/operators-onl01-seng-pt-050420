3c6e0363d5ad3c9a5fc2f89e0885a44f40cdfc72
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-06 02:21:44 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,21 +1,14 @@
-def unsafe?(speed)
-	if speed < 40
-		"unsafe"
-	else
-		"safe"
-	end
-	if speed > 60
-		"unsafe"
-	else
-		"safe"
-	end
 
-speed = speed.now
-puts "This is not a safe speed" unless speed == 50
+def unsafe?(speed)
+if speed > 60 || speed < 40
+	return true;
+else
+	return false;
+end
+end
+
 
 
 def not_safe?(speed)
-	speed < 40 ? true : false
-	speed > 60 ? true : false
-end
+speed > 60 || speed < 40 ? true : false;
 end
