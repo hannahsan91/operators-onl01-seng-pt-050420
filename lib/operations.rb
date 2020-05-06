0402a6c8b4e817fd7b98d0ff9561d0aa0533a0d1
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-06 02:09:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,19 @@
 def unsafe?(speed)
-	speed < 40 ? "unsafe" : "safe"
-	speed > 60 ? "unsafe" : "safe"
-end
+	if speed < 40
+		"unsafe"
+	else
+		"safe"
+	end
+	if speed > 60
+		"unsafe"
+	else
+		"safe"
+	end
+		
 
 
 
 def not_safe?(speed)
-
+	speed < 40 ? "unsafe" : "safe"
+	speed > 60 ? "unsafe" : "safe"
 end
